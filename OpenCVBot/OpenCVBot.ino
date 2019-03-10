17d5ec675ecbdb034e4641bc87c9e85489f21bc4
Chayan Mistry <chayanforyou007@gmail.com>
2019-03-10 09:06:01 +0600

    Update OpenCVBot.ino

--- OpenCVBot/OpenCVBot.ino
+++ OpenCVBot/OpenCVBot.ino
@@ -1,130 +1,149 @@
-String bluetoothRead, Str_x, Str_p;
-int x, points;
-int length;
+#include <SoftwareSerial.h>
 
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+//Initialize global variables
+String bluetoothRead;
+unsigned short x, y, area;
+unsigned short strLength;
+
+//Configure motor pin
 int pwmMotorA = 11;
 int pwmMotorB = 10;
-int ForwardA = 8;
-int BackA = 9;
-int ForwardB = 6;
-int BackB = 7;
+int leftMotorA = 9;
+int leftMotorB = 8;
+int rightMotorA = 7;
+int rightMotorB = 6;
 
+//Set motor speed
+int motorSpeed = 140;
 
 void setup() {
+
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
+  Serial.println("OpenCV Bot");
 
+  // Set the baud rate of (HC-05) Bluetooth module
+  mySerial.begin(38400);
+
+  //Setup pins
   pinMode(pwmMotorA, OUTPUT);
-  pinMode(ForwardA, OUTPUT);
+  pinMode(pwmMotorB, OUTPUT);
 
-  pinMode(BackA, OUTPUT);
-  pinMode(pwmMotorA, OUTPUT);
+  pinMode(leftMotorA, OUTPUT);
+  pinMode(leftMotorB, OUTPUT);
 
-  pinMode(ForwardB, OUTPUT);
-  pinMode(BackB, OUTPUT);
+  pinMode(rightMotorA, OUTPUT);
+  pinMode(rightMotorB, OUTPUT);
 
-  analogWrite(pwmMotorA, 140);
-  analogWrite(pwmMotorB, 140);
+  analogWrite(pwmMotorA, motorSpeed);
+  analogWrite(pwmMotorB, motorSpeed);
 }
 
 void loop() {
-  int i = 0;
-  char commandbuffer[200];
+  register byte ndx = 0;
+  char commandbuffer[50];
 
-  if (Serial.available()) {
+  if (mySerial.available() > 0) {
     delay(10);
-    while ( Serial.available()) {
-      commandbuffer[i++] = Serial.read();
+    while ( mySerial.available() && ndx < 50) {
+      commandbuffer[ndx++] = mySerial.read();
     }
 
-    commandbuffer[i++] = '\0';
+    commandbuffer[ndx] = '\0';
     bluetoothRead = (char*)commandbuffer;
-    length = bluetoothRead.length();
+    strLength = bluetoothRead.length();
+    //Serial.println(bluetoothRead);
 
     if (bluetoothRead.substring(0, 1).equals("X")) {
-      int i = 1;
+      uint8_t pos, i = 1;
 
       while (bluetoothRead.substring(i, i + 1) != ("Y")) {
         i++;
       }
 
-      Str_x = bluetoothRead.substring(1, i);
-      x = Str_x.toInt();
-//      Serial.print("X: ");
-//      Serial.println(x);
+      x = bluetoothRead.substring(1, i).toInt();
+      //Serial.print("X: ");
+      //Serial.println(x);
 
-      Str_p = bluetoothRead.substring(length - 1, length);
-      points = Str_p.toInt();
-//      Serial.print("Points: ");
-//      Serial.println(points);
+      pos = i + 1;
+      while (bluetoothRead.substring(i, i + 1) != ("A")) {
+        i++;
+      }
 
-      i = 1;
+      y = bluetoothRead.substring(pos, i).toInt();
+      //Serial.print("Y: ");
+      //Serial.println(y);
 
-      if (x < 80) {
+      area = bluetoothRead.substring(i + 1, strLength).toInt();
+      //Serial.print("Area: ");
+      //Serial.println(area);
+
+      if (x < 750) {
         Serial.println("Left");
         Left();
       }
-      if (x > 256) {
+      if (x > 1100) {
         Serial.println("Right");
         Right();
       }
-      if (x < 256 && x > 80) {
-        if (points == 1) {
+      if (x >= 750 && x <= 1100) {
+        if (area < 150) {
           Serial.println("Forward");
           Forward();
         }
-        if (points == 0) {
+        if (area >= 150 && area <= 250) {
           Serial.println("Stop");
           Stop();
         }
-        if (points == 2) {
+        if (area > 250) {
           Serial.println("Back");
           Back();
         }
       }
     }
   }
+  // all data has been sent, and the buffer is empty.
+  Serial.flush();
 }
 
 void Left() {
-  digitalWrite(ForwardA, LOW);
-  digitalWrite(BackA, HIGH);
+  digitalWrite(leftMotorA, LOW);
+  digitalWrite(leftMotorB, HIGH);
 
-  digitalWrite(ForwardB, HIGH);
-  digitalWrite(BackB, LOW);
+  digitalWrite(rightMotorA, HIGH);
+  digitalWrite(rightMotorB, LOW);
 }
-
 
 void Right() {
-  digitalWrite(ForwardA, HIGH);
-  digitalWrite(BackA, LOW);
+  digitalWrite(leftMotorA, HIGH);
+  digitalWrite(leftMotorB, LOW);
 
-  digitalWrite(ForwardB, LOW);
-  digitalWrite(BackB, HIGH);
+  digitalWrite(rightMotorA, LOW);
+  digitalWrite(rightMotorB, HIGH);
 }
-
 
 void Forward() {
-  digitalWrite(ForwardA, HIGH);
-  digitalWrite(BackA, LOW);
+  digitalWrite(leftMotorA, HIGH);
+  digitalWrite(leftMotorB, LOW);
 
-  digitalWrite(ForwardB, HIGH);
-  digitalWrite(BackB, LOW);
+  digitalWrite(rightMotorA, HIGH);
+  digitalWrite(rightMotorB, LOW);
 }
-
 
 void Back() {
-  digitalWrite(ForwardA, LOW);
-  digitalWrite(BackA, HIGH);
+  digitalWrite(leftMotorA, LOW);
+  digitalWrite(leftMotorB, HIGH);
 
-  digitalWrite(ForwardB, LOW);
-  digitalWrite(BackB, HIGH);
+  digitalWrite(rightMotorA, LOW);
+  digitalWrite(rightMotorB, HIGH);
 }
 
-
 void Stop() {
-  digitalWrite(ForwardA, LOW);
-  digitalWrite(BackA, LOW);
+  digitalWrite(leftMotorA, LOW);
+  digitalWrite(leftMotorB, LOW);
 
-  digitalWrite(ForwardB, LOW);
-  digitalWrite(BackB, LOW);
+  digitalWrite(rightMotorA, LOW);
+  digitalWrite(rightMotorB, LOW);
 }
